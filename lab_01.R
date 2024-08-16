65977f9a104c925347c2de7f1b3bfa04006dfc6d
rikharigaurav <141213738+rikharigaurav@users.noreply.github.com>
2024-08-16 15:18:47 +0530

    basic funcitons

---
+++ lab_01.R
@@ -0,0 +1,39 @@
+# Demonstrate the use of print method
+print("Hello, World!")
+
+# Perform Addition, Multiplication, Subtraction and Division on two numbers
+perform_operations <- function (num1, num2, operation){
+    if(operation == 1){
+        return (num1 + num2)
+    } else if (operation == 2){
+        return (num1 - num2)
+    } else if (operation == 3){
+        return (num1 * num2)
+    } else if (operation == 4){
+        return (num1 / num2)
+    } else {
+        return ("Invalid Operation")
+    }
+}
+
+
+num1 <- readline(prompt = "Enter value for num1: ")
+num2 <- readline(prompt = "Enter value for num2: ")
+
+cat("
+Value of Operation
+    1. Addition 
+    2. Subtraction 
+    3.  Multiplication 
+    4.  Division 
+
+")
+
+operation <- readline(prompt = "Enter value for operation: ")
+print(perform_operations(as.numeric(num1), as.numeric(num2), as.integer(operation)))
+
+
+# Print all the numbers from 0 to 100
+for ( x in 0 : 100){
+    print(x)
+}
